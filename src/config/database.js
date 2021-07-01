6a5f57c8368f1aa7e084ab6f111e14937d9ed837
Henrique Schmitt <henriquehschmitt@hotmail.com>
2021-07-01 20:52:10 -0300

    feat: adding database configs

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dialect : 'postgres',
+    host : 'localhost',
+    username : 'postgres',
+    password: '123456',
+    database: 'company',
+    define : {
+        timestamps : true,
+    }
+}

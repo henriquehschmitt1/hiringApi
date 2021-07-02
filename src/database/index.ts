8038c5a49ac565007abff776bd50b9b37433777d
Henrique Schmitt <henriquehschmitt@hotmail.com>
2021-07-01 22:57:33 -0300

    feat: added model init

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,16 @@
+import Address from "../models/Address"
+import Company from "../models/Company"
+import Employee from "../models/Company"
+import CompanyEmployee from "../models/CompanyEmployee"
+
 const Sequelize = require('sequelize')
 const dbConfig = require('../config/database')
 
 const connection = new Sequelize(dbConfig)
 
+Address.init(connection)
+Company.init(connection)
+Employee.init(connection)
+CompanyEmployee.init(connection)
 
 export = connection

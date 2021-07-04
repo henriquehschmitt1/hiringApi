bf67953a1c9adc42b282732b83eecc45c458186d
Henrique Schmitt <henriquehschmitt@hotmail.com>
2021-07-04 03:03:55 -0300

    feat: added getCompany funcionality, to get all employees of a company by cnpj

--- src/controllers/companyController.ts
+++ src/controllers/companyController.ts
@@ -1,6 +1,9 @@
 import Addresses from "../models/Addresses"
 import Companies from "../models/Companies"
+import CompanyEmployees from "../models/CompanyEmployees"
+import Employees from "../models/Employees"
 import Validate from "../utils/validate"
+import GetFromLoop from "../utils/getFromLoop"
 
 export class CompanyController {
 
@@ -19,7 +22,37 @@ export class CompanyController {
     }
 
     async getCompany(req: any, res: any) {
+        const { cnpj } = req.query
+        try {
+            Validate.isValidCnpj(cnpj)
 
+            const company = await Companies.findOne({
+                where: {
+                    cnpj
+                }
+            })
+
+
+            const companyEmployee = await CompanyEmployees.findAll({
+                where: {
+                    companyId: company.dataValues.id
+                }
+            })
+
+            const employeeIds = GetFromLoop.getCompanyEmployeeIds(companyEmployee)
+
+            const employees = await Employees.findAll({
+                where: {
+                    id: employeeIds
+                }
+            })
+
+            const employeeArray = GetFromLoop.getEmployees(employees)
+
+            res.json({ companyName: company.dataValues.name, employees: employeeArray })
+        } catch (error) {
+            res.json({ error })
+        }
     }
 
 }

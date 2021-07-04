96e9b18f550344bb80b8dc4b5743d55cd0fda281
Henrique Schmitt <henriquehschmitt@hotmail.com>
2021-07-03 23:18:45 -0300

    feat: added company controller

--- src/controllers/companyController.ts
+++ src/controllers/companyController.ts
@@ -1,13 +1,20 @@
-import Address from "../models/Address"
-import Company from "../models/Company"
+import Addresses from "../models/Addresses"
+import Companies from "../models/Companies"
+import Validate from "../utils/validate"
+
 export class CompanyController {
 
     async createCompany(req: any, res: any) {
         const { name, cnpj, zipCode, street, city, state, additionalAddressData } = req.body
         try {
-            console.log("dsa123das12d1sa2")
+            Validate.isValidCompany(name, cnpj, zipCode, street, city, state, additionalAddressData)
+
+            const address = await Addresses.create({ zipCode, street, city, state, additionalAddressData })
+            const company = await Companies.create({ name, cnpj, addressId: address.dataValues.id })
+
+            res.json({ company, address })
         } catch (error) {
-            console.log(error)
+            res.json({ error })
         }
     }
 

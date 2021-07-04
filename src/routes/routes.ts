c78b8323b71f7b21110ab6f410fadab334e926a5
Henrique Schmitt <henriquehschmitt@hotmail.com>
2021-07-04 01:10:34 -0300

    feat: added new post route to assign employees to companies

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,15 +1,19 @@
 import express from 'express'
 import { CompanyController } from '../controllers/companyController'
 import { EmployeeController } from '../controllers/employeeController'
+import { AssignController } from '../controllers/assignController'
 
 const companyController = new CompanyController()
 const employeeController = new EmployeeController()
+const assignController = new AssignController()
 
 const router = express.Router()
 
 router.post('/company', companyController.createCompany)
 
 router.post('/employee', employeeController.createEmployee)
+
+router.post('/assign', assignController.companyEmployee)
 
 //3 posts, cadastrar empregado, empresa e um no outro
 //2 gets, ao fazer get para empresa mostrar todos os funcionarios associados.
